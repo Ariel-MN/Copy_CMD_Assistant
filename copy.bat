2d5a62c47a90e6f4771311ab3eacd842e3533157
Ariel Montes <arielmontes1989@gmail.com>
2021-01-21 17:08:08 +0100

    Stable v.1

--- copy.bat
+++ copy.bat
@@ -1,119 +1,167 @@
 @echo off
 
-::     INFORMACION DEL CREADOR
-REM      NOMBRE:  Ariel Montes
-REM      WEBSITE: montesariel.com
+::     ABOUT ME
+REM      Name:  Ariel Montes
+REM      Website: montesariel.com
 
-::     REQUERIMIENTOS
-REM      Sistema Operativo Windows
+::     INFORMATION
+REM      The utility of this script is to copy all the files from a specific directory and its
+REM      subdirectories to a second given directory, separating the files into folders of a
+REM      specific dimension without compromising the integrity of the files or cutting them.
+REM      How? after copying each file it renames it to avoid being overwritten in case more
+REM      files share the same name in other subdirectories and it is also able to deal with
+REM      file names that use spaces or other special characters.
 
-::     INSTRUCCIONES DE USO
-REM      Es aconsejable cambiar los nombres de las imagenes a copiar "ctrl+a > f2 > del > enter"
-REM      y luego de nuevo "ctrl+a f2 > flecha-izquierda > del" esto hara que todas las
-REM      imagenes tengan por nombre un numero y evitar conflictos con los nombres de archivo.
+::     REQUIREMENTS
+REM      Windows Operating System
 
-::     INFORMACION
-REM      Los test han sido efectuados en Windows 10 Pro - Version 20H2.
-REM      Para que el programa funcione correctamente es necesario que los nombres de los
-REM      archivos no contengan ni espacios ni caracteres que no sean de tipo ASCII.
-REM      El control de los nombres de archivos deberia ser objeto de una futura
-REM      mejora o implementacion del codigo existente.
-
+::     DETAILS
+REM      The tests have been carried out in Windows 10 Pro - Version 20H2.
+REM      The only case in which a folder can exceed the maximum size set is if a file exceeds 
+REM      that size, to preserve its integrity it will be copied to a new folder.
 
 ::     DEBUG
-REM Previene que el programa se cierre en caso de error permitiendo leer los mensajes
+REM      Prevents the program from closing in the event of an error allowing messages to be read
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 
 
-::     CONFIGURACION
-REM Dimencion de archivos deseada por carpeta en bytes binary, valor actual 50MB
+::     SETTINGS
+REM Desired dimension per folder (current value 50 MB in binary bytes)
 set maxSize=52428800
-REM Directorio de las imagenes
-set source=D:\Directorio\Proveniencia\Imagenes
-REM Directorio donde copiar los archivos
-set dest=D:\Directorio\Destino
-REM Nombre de las carpetas generadas
-set folder=img_
+REM Directory of files to copy
+set source=
+REM Directory where to copy the files
+set dest=
+REM Name prefix for generated folders (followed by an increasing number)
+set FolderName=dir_
+REM Name prefix for copied files (followed by an increasing number)
+set FileName=file_
 
 
-::     PROGRAMA
-REM Archivos a copiar
-set files=
-REM Suma de archivos en bytes
-set size=0
-REM Dimencion carpeta creada
-set fdrSize=0
-REM Numero de carpeta
-set fNum=0
-
+::     PROGRAM
 setlocal EnableDelayedExpansion
 
-REM Itera en los diferentes archivos del directorio
-for %%f in (%source%\*) do (
+REM Iterate in the different files of the directory and its subdirectories
+for /f "delims=" %%f in ('dir /a-d /b /s %source%') do (
+
+    REM Current sum of files in byte
+    set CurrentSize=!size!
 	
-	REM Suma de archivos en byte temporal
-	set tSize=0
-	REM Bytes del archivo actual
-	set fSize=0
-	REM Suma de archivos en byte actual
-	set cSize=!size!
+    REM Control of file sizes in bytes
+    for /f "usebackq delims=" %%s in ('%%f') do (  
+        REM Current file size
+        set FileSize=%%~zs
+        if !FileSize! LSS %maxSize% (
+            REM Sum of all files (temporary)
+            set /a TemporalSize=!size!+!FileSize!
+            REM Sum of all files (definitive)
+            if !TemporalSize! LSS %maxSize% (set /a size+=!FileSize!)
+        )
+    )
 	
-	REM Control bytes de archivos
-	for /F "usebackq tokens=* delims=" %%s in ('%%f') do (	
-		REM Bytes del archivo actual
-		set /a fSize=%%~zs
-		if !fSize! LSS %maxSize% (
-			REM Suma temporal de los bytes
-			set /a tSize=!size!+!fSize!
-			REM Suma definitiva de los bytes
-			if !tSize! LSS %maxSize% (set /a size+=!fSize!)
+	REM If the dimension of the files and the current file are less than the specified maximum
+    if !TemporalSize! LSS %maxSize% (
+        if !FileSize! LSS %maxSize% (
+            REM Add filename to list
+            if "!FileList!" EQU "" (set FileList="%%f") else (set FileList=!FileList! "%%f")
 		)
-	)
+    )
 	
-	REM Si la dimencion de los archivos y del archivo actual son inferiores al maximo especificado
-	if !tSize! LSS %maxSize% (
-		if !fSize! LSS %maxSize% (
-			REM Suma nombre de archivo a la lista
-			set files=!files! "%%~nxf"
+	REM If the dimension of the files is greater than the specified maximum
+    if !TemporalSize! GTR %maxSize% (
+        set /a FolderNumb+=1
+		REM Create new folder
+		if not exist "%dest%\%FolderName%!FolderNumb!" mkdir "%dest%\%FolderName%!FolderNumb!"
+        set /a fdrSize=!CurrentSize!/1048576
+        echo._________________________________
+        echo Folder: %FolderName%!FolderNumb!  Size: ~!fdrSize!MB
+        echo.
+		REM Copy the files
+		set CopyNumb=0
+		for %%i in (!FileList!) do (
+			for /f "tokens=*" %%o in ('copy %%i "%dest%\%FolderName%!FolderNumb!"') do (
+				set output=%%o
+				set status=!output:~0,1!
+				if /i !status! EQU 1 (
+					set /a CopyNumb+=1
+					set /a FileNumb+=1
+					if not exist "%dest%\%FolderName%!FolderNumb!\!FileName!!FileNumb!%%~xi" (
+						REM Rename file
+						ren "%dest%\%FolderName%!FolderNumb!\%%~ni%%~xi" "!FileName!!FileNumb!.*"
+					) else (
+						echo Could not rename file "%%~ni%%~xi" to "!FileName!!FileNumb!%%~xi" because "!FileName!!FileNumb!%%~xi" already exist in folder "%FolderName%!FolderNumb!"
+					)
+				) else (echo !output!: %%i)
+			)
 		)
-	)
+		if /i !CopyNumb! GTR 1 echo !CopyNumb! files copied
+		if /i !CopyNumb! EQU 1 echo 1 file copied
+		
+        REM Reset values
+        set FileList=
+        set size=0
+        REM Save the current file that exceeds
+        if !FileSize! LSS %maxSize% (
+            set FileList="%%f"
+            set size=!FileSize!
+        )
+    )
 	
-	REM Si la dimencion de los archivos es superior al maximo especificado
-	if !tSize! GTR %maxSize% (
-		REM Copia los archivos
-		set /a fNum+=1
-		set /a fdrSize=!cSize!/1048576
-		echo._________________________________
-		echo folder %folder%!fNum!  size ~!fdrSize!MB
-		robocopy %source% %dest%\%folder%!fNum! !files! /NJH /NJS /nc /ns | find /v "\"
-		REM Reinicia valores
-		set files=
-		set size=0
-		REM Reinicia valores guardando archivo actual que excede
-		if !fSize! LSS %maxSize% (
-			set files="%%~nxf"
-			set size=!fSize!
+	REM If the current file dimension is greater than the specified maximum
+    if !FileSize! GTR %maxSize% (
+        set /a FolderNumb+=1
+		if not exist "%dest%\%FolderName%!FolderNumb!" mkdir "%dest%\%FolderName%!FolderNumb!"
+        set /a fdrSize=!FileSize!/1048576
+        echo._________________________________
+        echo Folder: %FolderName%!FolderNumb!  Size: ~!fdrSize!MB
+		echo.
+		for %%i in (%%f) do (
+			REM Copy the current file to a separate folder
+			for /f "tokens=*" %%o in ('copy "%%f" "%dest%\%FolderName%!FolderNumb!"') do (
+				set output=%%o
+				set status=!output:~0,1!
+				if /i !status! EQU 1 (
+					echo 1 file copied
+					set /a FileNumb+=1
+					if not exist "%dest%\%FolderName%!FolderNumb!\!FileName!!FileNumb!%%~xi" (
+						REM Rename file
+						ren "%dest%\%FolderName%!FolderNumb!\%%~ni%%~xi" "!FileName!!FileNumb!.*"
+					) else (
+						echo Could not rename file "%%~ni%%~xi" to "!FileName!!FileNumb!%%~xi" because "!FileName!!FileNumb!%%~xi" already exist in folder "%FolderName%!FolderNumb!"
+					)
+				) else (echo !output!: %%i)
+			)
 		)
-	)
-	
-	REM Si la dimencion del archivo actual es superior al maximo especificado
-	if !fSize! GTR %maxSize% (
-		REM Copia el archivo actual en una carpeta aparte
-		set /a fNum+=1
-		set /a fdrSize=!fSize!/1048576
-		echo._________________________________
-		echo folder %folder%!fNum!  size ~!fdrSize!MB
-		robocopy %source% %dest%\%folder%!fNum! "%%~nxf" /NJH /NJS /nc /ns | find /v "\"
-	)
+    )
 )
 
-REM Copia los archivos restantes al terminar el loop
-if "%files%" NEQ "" (
-	set /a fNum+=1
-	set /a fdrSize=!size!/1048576
-	echo._________________________________
-	echo folder %folder%!fNum!  size ~!fdrSize!MB
-	robocopy %source% %dest%\%folder%!fNum! !files! /NJH /NJS /nc /ns | find /v "\"
+REM Copy the remaining files at the end of the loop
+if "%FileList%" NEQ "" (
+    set /a FolderNumb+=1
+	if not exist "%dest%\%FolderName%!FolderNumb!" mkdir "%dest%\%FolderName%!FolderNumb!"
+    set /a fdrSize=!size!/1048576
+    echo._________________________________
+    echo Folder: %FolderName%!FolderNumb!  Size: ~!fdrSize!MB
+	echo.
+	set CopyNumb=0
+	for %%i in (!FileList!) do (
+		for /f "tokens=*" %%o in ('copy %%i "%dest%\%FolderName%!FolderNumb!"') do (
+			set output=%%o
+			set status=!output:~0,1!
+			if /i !status! EQU 1 (
+				set /a CopyNumb+=1
+				set /a FileNumb+=1
+				if not exist "%dest%\%FolderName%!FolderNumb!\!FileName!!FileNumb!%%~xi" (
+					REM Rename file
+					ren "%dest%\%FolderName%!FolderNumb!\%%~ni%%~xi" "!FileName!!FileNumb!.*"
+				) else (
+					echo Could not rename file "%%~ni%%~xi" to "!FileName!!FileNumb!%%~xi" because "!FileName!!FileNumb!%%~xi" already exist in folder "%FolderName%!FolderNumb!"
+				)
+			) else (echo !output!: %%i)
+		)
+	)
+	if /i !CopyNumb! GTR 1 echo !CopyNumb! files copied
+	if /i !CopyNumb! EQU 1 echo 1 file copied
 )
 
 endlocal
